5bfe3ff75d3392332f7fed094d139affc564f98c
belalzz <belal.khaled041@gmail.com>
2025-05-18 07:50:48 +0300

    final

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from GUI.MainWindow import run
+import joblib
 
-def main() -> None:
-    run()
+def main():
+    model = joblib.load("model.joblib")
+    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
